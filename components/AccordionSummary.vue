cc5786a2f535170716cabbeb9772647c182b0038
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2024-06-09 18:21:16 -0400

    Moved default comments + scroll

--- components/AccordionSummary.vue
+++ components/AccordionSummary.vue
@@ -52,39 +52,6 @@ const dataSommaire = {
     "Suzanne Bougie – 1255, rue Dion, Val-David (Québec) J0N 2N0 (514) 892-3106 – suzannebougie13@gmail.com",
     "Réal Burelle – 218-1811, rue de Clignancourt, Saint-Jérôme (Québec) (438) 988-1024 – realburelle@hotmail.com",
   ],
-  feedback: [
-    {
-      name: "Jean-Luc Hétu",
-      comments: [
-        "Me voici soulagé, car je redoutais que l’ivresse de votre amour naissant n’éclabousse des endeuillés encore aux prises avec leur chagrin ou toujours à la recherche de l’âme sœur. C’est la sobriété de la narration qui concourt à mon soulagement.",
-        "Vous vous exprimez spontanément, sans filtre, mais sans vous laisser entraîner par votre plaisir évident d’écrire. En plus de l’expression claire et du ton naturel, cette sobriété contribue à garder son élan à votre récit et à soutenir l’intérêt. Toutefois, cela ne m’a pas empêché de verser quelques larmes lors de l’évocation des circonstances ayant entouré le décès de votre conjoint, de votre conjointe.",
-        "J’aime la liberté avec laquelle vous faites référence à Richard et à Micheline tout au long de votre récit. J’y ai découvert des scènes attendrissantes et fort belles. Ces références font partie intégrante de votre projet et vous les faites avec tact.",
-        "Les poèmes sont bien choisis et offrent un écho senti de votre histoire.",
-      ],
-    },
-    {
-      name: "Jocelyne Thifault",
-      comments: [
-        "J’ai été émue et captivée par cette unique introspection à deux voix qui se lit aisément grâce à une écriture remarquable, vivante, rythmée, imagée. Prose et poésie se relancent et se complètent tout au long de ce cheminement de l’ombre à la lumière.",
-        "L’intimité partagée de ces deux endeuillés amoureux est touchante et porte à la réflexion. J’ai essuyé quelques larmes en lisant des passages poignants, mais j’ai ri aussi devant les hésitations et les doutes de ces deux « ados » septuagénaires redécouvrant l’amour. Bravo!",
-      ],
-    },
-    {
-      name: "Jocelyne Aird-Bélanger",
-      comments: [
-        "Votre témoignage est absolument convaincant. Le récit des décès de vos partenaires fait pleurer, mais nous sommes vite réconfortés par les moments de tendresse et d’amour authentiques qui jalonnent votre parcours.",
-        "L’émerveillement que vous vivez grâce à cette nouvelle histoire d’amour, malgré les souvenirs pénibles en arrière-fond, est fort inspirant. Votre écriture à tous les deux est belle, riche et agréable à lire.",
-      ],
-    },
-    {
-      name: "Marielle Beauregard",
-      comments: [
-        "Quelle originalité que de nous présenter vos premiers courriels, vos douces correspondances. J’ai aimé lire vos amours passées et nouvelles. Merci de nous décrire vos corps qui vibrent, pourquoi diantre cesseraient-ils? En tous cas, comme vous, moi, j’y crois. Vive les ressacs océaniques!",
-        "Réal, quels magnifiques poèmes et quelle délicatesse t’habite. Suzanne, ce que je suis de nouveau chavirée en lisant les trois extraits de ton Témoignage d’une endeuillée. Quelle superbe idée!",
-        "Je vous remercie d’avoir partagé vos états d’âme, votre belle écriture, vos talents respectifs et surtout votre sensibilité. Longue vie à vous deux dans l’intensité.",
-      ],
-    },
-  ],
 };
 
 export default {
@@ -198,27 +165,12 @@ export default {
       </v-expansion-panel-text>
     </v-expansion-panel>
 
-    <v-expansion-panel>
+    <v-expansion-panel id="start-comments">
       <v-expansion-panel-title @click.capture.stop="openPanel(3)"
         >Commentaires</v-expansion-panel-title
       >
       <v-expansion-panel-text>
-        <v-list lines="one" id="default-comments" v-if="page === 1">
-          <v-list-item v-for="(comment, i) in dataSommaire.feedback" :key="i">
-            <v-card variant="tonal">
-              <v-card-title style="text-align: left">{{
-                comment.name
-              }}</v-card-title>
-              <v-card-text
-                v-for="(paragraph, j) in comment.comments"
-                :key="j"
-                :class="j !== 0 ? 'comments-text' : null"
-              >
-                {{ paragraph }}
-              </v-card-text>
-            </v-card>
-          </v-list-item>
-        </v-list>
+        
         <comments :page="page" @update:page="page = $event" />
       </v-expansion-panel-text>
     </v-expansion-panel>
@@ -277,10 +229,6 @@ export default {
 
 .comments-text {
   padding-top: 0;
-}
-
-#default-comments {
-  padding-bottom: 0;
 }
 
 #read-button {
--- components/Comments.vue
+++ components/Comments.vue
@@ -5,6 +5,40 @@ export default {
       cusdisAppId: import.meta.env.VITE_CUSDIS_APP_ID,
       comments: [],
       pageValue: this.page,
+      numberOfComments: 4,
+      defaultComments: [
+        {
+          name: "Marielle Beauregard",
+          comments: [
+            "Quelle originalité que de nous présenter vos premiers courriels, vos douces correspondances. J’ai aimé lire vos amours passées et nouvelles. Merci de nous décrire vos corps qui vibrent, pourquoi diantre cesseraient-ils? En tous cas, comme vous, moi, j’y crois. Vive les ressacs océaniques!",
+            "Réal, quels magnifiques poèmes et quelle délicatesse t’habite. Suzanne, ce que je suis de nouveau chavirée en lisant les trois extraits de ton Témoignage d’une endeuillée. Quelle superbe idée!",
+            "Je vous remercie d’avoir partagé vos états d’âme, votre belle écriture, vos talents respectifs et surtout votre sensibilité. Longue vie à vous deux dans l’intensité.",
+          ],
+        },
+        {
+          name: "Jocelyne Aird-Bélanger",
+          comments: [
+            "Votre témoignage est absolument convaincant. Le récit des décès de vos partenaires fait pleurer, mais nous sommes vite réconfortés par les moments de tendresse et d’amour authentiques qui jalonnent votre parcours.",
+            "L’émerveillement que vous vivez grâce à cette nouvelle histoire d’amour, malgré les souvenirs pénibles en arrière-fond, est fort inspirant. Votre écriture à tous les deux est belle, riche et agréable à lire.",
+          ],
+        },
+        {
+          name: "Jocelyne Thifault",
+          comments: [
+            "J’ai été émue et captivée par cette unique introspection à deux voix qui se lit aisément grâce à une écriture remarquable, vivante, rythmée, imagée. Prose et poésie se relancent et se complètent tout au long de ce cheminement de l’ombre à la lumière.",
+            "L’intimité partagée de ces deux endeuillés amoureux est touchante et porte à la réflexion. J’ai essuyé quelques larmes en lisant des passages poignants, mais j’ai ri aussi devant les hésitations et les doutes de ces deux « ados » septuagénaires redécouvrant l’amour. Bravo!",
+          ],
+        },
+        {
+          name: "Jean-Luc Hétu",
+          comments: [
+            "Me voici soulagé, car je redoutais que l’ivresse de votre amour naissant n’éclabousse des endeuillés encore aux prises avec leur chagrin ou toujours à la recherche de l’âme sœur. C’est la sobriété de la narration qui concourt à mon soulagement.",
+            "Vous vous exprimez spontanément, sans filtre, mais sans vous laisser entraîner par votre plaisir évident d’écrire. En plus de l’expression claire et du ton naturel, cette sobriété contribue à garder son élan à votre récit et à soutenir l’intérêt. Toutefois, cela ne m’a pas empêché de verser quelques larmes lors de l’évocation des circonstances ayant entouré le décès de votre conjoint, de votre conjointe.",
+            "J’aime la liberté avec laquelle vous faites référence à Richard et à Micheline tout au long de votre récit. J’y ai découvert des scènes attendrissantes et fort belles. Ces références font partie intégrante de votre projet et vous les faites avec tact.",
+            "Les poèmes sont bien choisis et offrent un écho senti de votre histoire.",
+          ],
+        },
+      ],
     };
   },
   created() {
@@ -22,18 +56,42 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           if (data.data.data.length === 0) {
+            this.addDefaultComments();
             return;
           }
           const sortedComments = data.data.data.sort(
             (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
           );
           this.comments = [...this.comments, sortedComments];
+          this.numberOfComments += data.data.data.length;
           this.getComments(counter + 1);
         });
     },
     updatePage(page) {
       this.pageValue = page;
+
       this.$emit("update:page", page);
+    },
+    addDefaultComments() {
+      while (this.comments[this.comments.length - 1].length < 10) {
+        let feedbackComment = this.defaultComments.pop();
+        let adjustedComment = {
+          id: feedbackComment.id,
+          by_nickname: feedbackComment.name,
+          content: feedbackComment.comments,
+        };
+        this.comments[this.comments.length - 1].push(adjustedComment);
+      }
+      const lastComments = [];
+      for (let i = 0; i < this.defaultComments.length; i++) {
+        let adjustedComment = {
+          id: this.defaultComments[i].id,
+          by_nickname: this.defaultComments[i].name,
+          content: this.defaultComments[i].comments,
+        };
+        lastComments.push(adjustedComment);
+      }
+      if (lastComments.length !== 0) this.comments.push(lastComments);
     },
   },
   props: {
@@ -44,9 +102,15 @@ export default {
   },
   watch: {
     pageValue: {
-      immediate: true,
+      immediate: false,
+      deep: true,
       handler(newValue) {
         this.$emit("update:page", newValue);
+        this.$nextTick(() => {
+          const element = document.getElementById("start-comments");
+          if (element)
+            element.scrollIntoView({ behavior: "smooth", block: "start" });
+        });
       },
     },
   },
@@ -54,17 +118,31 @@ export default {
 </script>
 
 <template>
+  <p style="text-align: right; color: grey; padding-right: 16px;">{{ numberOfComments + " commentaires" }}</p>
   <v-list id="comments-top">
-    <v-list-item v-for="comment in comments[pageValue - 1]" :key="comment.id">
-      <v-card variant="tonal">
+    <v-list-item
+      v-for="(comment, index) in comments[pageValue - 1]"
+      :key="comment.id"
+    >
+      <v-card variant="tonal" :id="index === 0 ? 'first-comment' : 'other'">
         <v-card-title>{{ comment.by_nickname }}</v-card-title>
-        <v-card-subtitle>{{
+        <v-card-subtitle v-if="comment.createdAt">{{
           new Date(comment.createdAt).toLocaleString()
         }}</v-card-subtitle>
-        <v-card-text id="comment">{{ comment.content }}</v-card-text>
+
+        <div v-if="Array.isArray(comment.content)" id="comments-text">
+          <v-card-text
+            id="comments-text"
+            v-for="(para, index) in comment.content"
+            :key="index"
+            >{{ para }}</v-card-text
+          >
+        </div>
+        <v-card-text id="comment" v-else>{{ comment.content }}</v-card-text>
       </v-card>
     </v-list-item>
   </v-list>
+
   <v-pagination v-model="pageValue" :length="comments.length"></v-pagination>
 </template>
 
@@ -78,6 +156,14 @@ export default {
 }
 
 #comments-top {
+  padding-top: 0;
+}
+
+#default-comments {
+  padding-bottom: 0;
+}
+
+#comments-text {
   padding-top: 0;
 }
 </style>
